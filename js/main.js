f96b1367e39cd6d66089d35ede2062026f4165e7
Chan0369 <xbetty_chanx@outlook.com>
2016-03-02 19:47:53 -0500

    Added two variables and 4 loops

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var letterLooper = prompt('Enter a letter:');
+
+var i;
+
+for (var i = 0; i < 10; i++) {
+  document.write(letterLooper + '<br>');
+  for (var i = 0; i < 9; i++) {
+    document.write(letterLooper + letterLooper + '<br>');
+    for (var i = 0; i < 8; i++) {
+      document.write(letterLooper + letterLooper + letterLooper + '<br>');
+      for (var i = 0; i < 7; i++) {
+        document.write(letterLooper + letterLooper + letterLooper + letterLooper + '<br>');
+      }
+    }
+  }
+}
